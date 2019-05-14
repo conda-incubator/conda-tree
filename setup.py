dd1a120c7076292f6ad071cb6321b68ba775fd91
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-05-14 11:06:46 +0200

    Add setup.py
    This will support Windows properly and allow conda-tree to be
    added to conda-forge.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import shutil
+
+
+# we need to rename the script because it's not a valid module name
+shutil.copyfile('conda-tree.py', 'conda_tree.py')
+
+setup(name='conda-tree',
+      version='0.0.3',
+      description='conda dependency tree helper',
+      author='Renan Valieris',
+      url='https://github.com/rvalieris/conda-tree',
+      py_modules=['conda_tree'],
+      entry_points={
+        'console_scripts': ['conda-tree=conda_tree:main']})
