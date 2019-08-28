706d643e6b448e8c94171662248d4d189e126101
Léopold Mebazaa <lemeb@users.noreply.github.com>
2019-08-28 20:44:31 +0200

    0.0.5 proposal

--- README.md
+++ README.md
@@ -24,6 +24,16 @@ $ conda-tree leaves
 $ conda-tree depends samtools
 ['curl', 'xz', 'libgcc', 'zlib']
 
+# dependencies in a tree form
+$ conda-tree depends sqlite
+sqlite==3.29.0
+  - ncurses [required: >=6.1,<6.2.0a0, installed: 6.1]
+    - libcxx [required: >=4.0.1, installed: 8.0.1]
+      - libcxxabi [required: 8.0.1, 0, installed: 8.0.1]
+  - readline [required: >=8.0,<9.0a0, installed: 8.0]
+    - ncurses [required: >=6.1,<6.2.0a0, installed: 6.1]
+      ... (already above)
+
 # which packages depend on a specific package
 $ conda-tree whoneeds xz
 ['samtools', 'bcftools', 'htslib', 'python']
@@ -32,6 +42,20 @@ $ conda-tree whoneeds xz
 $ conda-tree cycles
 pip -> python -> pip
 pip -> wheel -> python -> pip
+
+# full dependency tree
+$ conda-tree deptree --full
+neovim==0.3.1
+  - pynvim [required: Any, installed: 0.3.2]
+    - greenlet [required: Any, installed: 0.4.15]
+    - msgpack-python [required: >=0.5.0, installed: 0.6.1]
+      - libcxx [required: >=4.0.1, installed: 8.0.1]
+        - libcxxabi [required: 8.0.1, 0, installed: 8.0.1]
+conda-tree==0.0.4
+  - conda [required: Any, installed: 4.7.11]
+    - conda-package-handling [required: >=1.3.0, installed: 1.4.1]
+      - libarchive [required: >=3.3.3, installed: 3.3.3]
+...
 
 # query a different conda prefix/env
 $ conda-tree -p /conda/envs/trinity leaves
--- conda-tree.py
+++ conda-tree.py
@@ -9,103 +9,161 @@ import subprocess
 import conda.exports
 import networkx
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 def get_local_cache(prefix):
-    return conda.exports.linked_data(prefix=prefix)
+	return conda.exports.linked_data(prefix=prefix)
 
 def get_package_key(cache, package_name):
-    ks = list(filter(lambda i: l[i]['name'] == package_name, l))
-    return ks[0]
+	ks = list(filter(lambda i: l[i]['name'] == package_name, l))
+	return ks[0]
 
-def make_cache_graph(cache):
+def make_cache_graph(cache, no_python):
 	g = networkx.DiGraph()
 	for k in cache.keys():
 		n = cache[k]['name']
-		g.add_node(n)
+		v = cache[k]['version']
+		# Removing python to avoid cycles (--tree option)
+		if no_python and n == "python": continue
+		g.add_node(n, version=v)
 		for j in cache[k]['depends']:
 			n2 = j.split(' ')[0]
-			g.add_edge(n, n2)
+			v2 = j.split(' ')[1:]
+			g.add_edge(n, n2, version=v2)
 	return(g)
 
 def print_graph_dot(g):
-    print("digraph {")
-    for k,v in g.edges():
-       print("  \"{}\" -> \"{}\"".format(k,v))
-    print("}")
+	print("digraph {")
+	for k,v in g.edges():
+	   print("  \"{}\" -> \"{}\"".format(k,v))
+	print("}")
 
 def remove_from_graph(g, node, _cache=None):
-    if _cache is None: _cache = {}
-    if node not in _cache:
-        _cache[node] = True
-        for k,v in g.out_edges(node):
-            g = remove_from_graph(g, v, _cache)
-    if node in g: g.remove_node(node)
-    return(g)
+	if _cache is None: _cache = {}
+	if node not in _cache:
+		_cache[node] = True
+		for k,v in g.out_edges(node):
+			g = remove_from_graph(g, v, _cache)
+	if node in g: g.remove_node(node)
+	return(g)
+
+def print_dependencies(g, pkg, parent, indent, args, treated):
+	s = "" # String to print
+	v = g.nodes[pkg]['version']
+	out_edges = g.out_edges(pkg)
+	e = [i[1] for i in out_edges]
+	# Removing python to avoid cycles
+	if "python" in e: e.remove("python")
+	if args.no_conda and "conda" in e: e.remove("conda")
+	if indent == 0:
+		s += f"{pkg}=={v}\n"
+	else:
+		r = ', '.join(g.edges[parent, pkg]['version'])
+		r = 'Any' if r == '' else r
+		i_str = '  ' * indent
+		s += f"{i_str}- {pkg} [required: {r}, installed: {v}]\n"
+		if pkg in treated and not args.full:
+			s += f"{i_str}  ... (already above)\n"
+			return s, treated
+		else:
+			if len(e) > 0: treated.add(pkg)
+	for pack in e: 
+		tree_str, treat = print_dependencies(g, pack, pkg, indent+1, args, treated)
+		s += tree_str
+		for pkg_name in treat: treated.add(pkg_name)
+	return s, treated
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p','--prefix', default=None)
-    parser.add_argument('-n','--name', default=None)
-    parser.add_argument('-v','--version', action='version', version='%(prog)s '+__version__)
-    parser.add_argument('-r','--recursive', help='show dependencies of dependencies',default=False, action='store_true')
-    subparser = parser.add_subparsers(dest='subcmd')
-    subparser.add_parser('leaves', help='shows leaf packages')
-    subparser.add_parser('cycles', help='shows dependency cycles')
-    p = subparser.add_parser('whoneeds', help='shows packages that depends on this package')
-    p.add_argument('package', help='the target package')
-    p = subparser.add_parser('depends', help='shows this package dependencies')
-    p.add_argument('package', help='the target package')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-p','--prefix', default=None)
+	parser.add_argument('-n','--name', default=None)
+	parser.add_argument('-v','--version', action='version', version='%(prog)s '+__version__)
 
-    if args.name is not None:
-        # Allow user to specify name, but check the environment for an
-        # existing CONDA_EXE command.  This allows a different conda
-        # package to be installed (and imported above) but will
-        # resolve the name using their expected conda.  (The imported
-        # conda here will find the environments, but might not name
-        # them as the user expects.)
-        _conda = os.environ.get('CONDA_EXE', 'conda')
-        _info = json.loads(subprocess.check_output(
-            [_conda, 'info', '-e', '--json']))
-        args.prefix = conda.base.context.locate_prefix_by_name(
-            name=args.name, envs_dirs=_info['envs_dirs'])
+	subparser = parser.add_subparsers(dest='subcmd')
+	subparser.add_parser('leaves', help='shows leaf packages')
+	subparser.add_parser('cycles', help='shows dependency cycles')
 
-    if args.prefix is None:
-        args.prefix = sys.prefix
-         
-    l = get_local_cache(args.prefix)
-    g = make_cache_graph(l)
+	package_cmds = argparse.ArgumentParser(add_help=False)
+	package_cmds.add_argument('package', help='the target package')
 
-    if args.subcmd == 'cycles':
-        for i in networkx.simple_cycles(g):
-            print(" -> ".join(i)+" -> "+i[0])
+	rec_or_tree = package_cmds.add_mutually_exclusive_group(required=False)
+	rec_or_tree.add_argument('-t', '--tree', help='show dependencies of dependencies in tree form, ignores python as a dependency to avoid cycles', default=False, action="store_true")
+	rec_or_tree.add_argument('-r','--recursive', help='show dependencies of dependencies',default=False, action='store_true')
 
-    elif args.subcmd == 'depends':
-        if args.package not in g:
-            print("warning: package \"%s\" not found"%(args.package), file=sys.stderr)
-        if args.recursive:
-            e = list(networkx.descendants(g, args.package))
-        else:
-            e = list(map(lambda i: i[1], g.out_edges(args.package)))
-        print(e)
+	hiding_cmds = argparse.ArgumentParser(add_help=False)
+	hiding_cmds.add_argument('--no-conda', help='does not show dependencies of conda to make the tree easier to understand', default=False, action='store_true')
+	hiding_cmds.add_argument('--full', help='shows the complete dependency tree, with all the repetitions that it entails', default=False, action='store_true')
 
-    elif args.subcmd == 'whoneeds':
-        if args.package not in g:
-            print("warning: package \"%s\" not found"%(args.package), file=sys.stderr)
-        if args.recursive:
-            e = list(networkx.ancestors(g, args.package))
-        else:
-            e = list(map(lambda i: i[0], g.in_edges(args.package)))
-        print(e)
+	subparser.add_parser('whoneeds', help='shows packages that depends on this package', parents=[package_cmds, hiding_cmds])
+	subparser.add_parser('depends', help='shows this package dependencies', parents=[package_cmds, hiding_cmds])
+	subparser.add_parser('deptree', help="shows the complete dependency tree ('python' is excluded to avoid cycles)", parents=[hiding_cmds])
 
-    elif args.subcmd == 'leaves':
-        e = list(map(lambda i:i[0],(filter(lambda i:i[1]==0,g.in_degree()))))
-        print(e)
-    else:
-        parser.print_help()
-        sys.exit(1)
+	args = parser.parse_args()
+
+	if args.name is not None:
+		# Allow user to specify name, but check the environment for an
+		# existing CONDA_EXE command.  This allows a different conda
+		# package to be installed (and imported above) but will
+		# resolve the name using their expected conda.  (The imported
+		# conda here will find the environments, but might not name
+		# them as the user expects.)
+		_conda = os.environ.get('CONDA_EXE', 'conda')
+		_info = json.loads(subprocess.check_output(
+			[_conda, 'info', '-e', '--json']))
+		args.prefix = conda.base.context.locate_prefix_by_name(
+			name=args.name, envs_dirs=_info['envs_dirs'])
+
+	if args.prefix is None:
+		args.prefix = sys.prefix
+		 
+	l = get_local_cache(args.prefix)
+	no_python = True if (args.subcmd == "deptree" or args.tree) else False
+	g = make_cache_graph(l, no_python)
+
+	def get_leaves(graph):
+		return list(map(lambda i:i[0],(filter(lambda i:i[1]==0,graph.in_degree()))))
+
+	if args.subcmd == 'cycles':
+		for i in networkx.simple_cycles(g):
+			print(" -> ".join(i)+" -> "+i[0])
+
+	elif args.subcmd == 'depends':
+		if args.package not in g:
+			print("warning: package \"%s\" not found"%(args.package), file=sys.stderr)
+		if args.recursive:
+			e = list(networkx.descendants(g, args.package))
+			print(e)
+		elif args.tree:
+			if networkx.is_directed_acyclic_graph(g):
+				tree, _ = print_dependencies(g, args.package, None, 0, args, set())
+				print(tree)
+		else:
+			e = list(map(lambda i: i[1], g.out_edges(args.package)))
+			print(e)
+
+	elif args.subcmd == 'whoneeds':
+		if args.package not in g:
+			print("warning: package \"%s\" not found"%(args.package), file=sys.stderr)
+		if args.recursive:
+			e = list(networkx.ancestors(g, args.package))
+		else:
+			e = list(map(lambda i: i[0], g.in_edges(args.package)))
+		print(e)
+
+	elif args.subcmd == 'leaves':
+		print(get_leaves(g))
+
+	elif args.subcmd == 'deptree':
+		treated = set()
+		complete_tree = ""
+		for pk in get_leaves(g):
+			tree, treated = print_dependencies(g, pk, None, 0, args, treated)
+			complete_tree += tree
+		print(''.join(complete_tree))
+	else:
+		parser.print_help()
+		sys.exit(1)
 
 if __name__ == "__main__":
-    main()
+	main()
 
